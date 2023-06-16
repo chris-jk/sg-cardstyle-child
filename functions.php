c9f94cddd8a9f7d5c8a8214bf8e78c5de9f40055
chris-jk <edetail@yahoo.com>
2023-06-16 12:45:52 -0700

    cleaned up unused files

--- functions.php
+++ functions.php
@@ -1,4 +1,31 @@
 <?php
+// works 
+function displaySVGFiles($fileName)
+{
+  // Get the current directory
+  $currentDir = __DIR__;
+
+  // Specify the folder name
+  $folderName = 'svgs';
+
+  // Build the folder path
+  $folderPath = $currentDir . '/' . $folderName;
+
+  // Construct the full file path
+  $filePath = $folderPath . '/' . $fileName . '.svg';
+
+  // Check if the file exists
+  if (file_exists($filePath)) {
+    // Display the SVG file as an image
+
+    return "<img src=\"$filePath\" alt=\"SVG $fileName\">";
+
+  } else {
+    return "<img src=\"\" alt=\"SVG $fileName\">";
+  }
+}
+
+// select from tags searchable
 function enqueue_select2_jquery()
 {
   wp_register_style('select2css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css');
@@ -15,7 +42,7 @@ function enqueue_select2_jquery()
 add_action('wp_enqueue_scripts', 'enqueue_select2_jquery');
 
 
-
+// add custom child theme support
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles()
 {
@@ -23,6 +50,7 @@ function my_theme_enqueue_styles()
   wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
 }
 
+// add custom feilds to posts content funtion
 function prefix_add_content($content)
 {
   $custom_fields_aka_ratings_type = array(
@@ -75,6 +103,7 @@ function prefix_add_content($content)
     'hcf_child_2'
   );
 
+  // check strain_type to add title to tables
   if (esc_attr(get_post_meta(get_the_ID(), 'hcf_strain_type', true))) {
     $title = get_the_title();
     $terp_profile = '<h3>' . $title . ' Terpene Profile</h3>';
@@ -90,13 +119,13 @@ function prefix_add_content($content)
   ;
 
 
-  // More custom field arrays as needed...
+  // prefix_generate_table 2 columns name:value
   $table_aka = prefix_generate_table($custom_fields_aka_ratings_type);
   $table_thc_cbg_cbd = prefix_generate_table($custom_fields_thc_cbg_cbd);
   $table_dom_terp = prefix_generate_table($custom_fields_dom_terp);
   $table_other_terp = prefix_generate_table($custom_fields_other_terp);
 
-  // Add original post content in between the tables
+  // prefix_generate_table_3 3 columns 
   $new_content .= $content;
   $table_flav = prefix_generate_table_3($custom_fields_flav);
   $table_feel = prefix_generate_table_3($custom_fields_feel);
@@ -174,26 +203,7 @@ function prefix_generate_table_3($fields)
 
     if ($field_value) {
       // Add different SVGs depending on the field value
-      switch (strtolower($field_value)) {
-        case 'sweet':
-          $svg = '<svg class="align-svg" width="20" height="20">
-                        <path d="M14.8 8c0-.3-.1-.6-.2-.8-.1-.4-.4-.7-.6-.9-.3-.3-.6-.5-1-.7-.1 0-.2-.1-.3-.1.1-.2.1-.3.3-.4.1-.1.3-.2.4-.3.2-.1.3-.1.5 0 .4.1.8-.2.8-.6.1-.4-.2-.8-.6-.8-.4-.1-.8 0-1.2.1s-.7.3-1 .6c-.3.3-.5.6-.6 1 0 .1-.1.3-.1.5-.1 0-.3.1-.4.1-.3.1-.6.3-.9.5-.3.3-.5.6-.6 1-.1.2-.2.5-.2.8-3 .8-4.8 2.6-4.8 5.1 0 .7.2 1.4.7 2 .3.4.7.7 1.2.9l.7 3.3c.1.4.3.8.7 1 .3.3.7.4 1.2.4h6.5c.4 0 .8-.1 1.2-.4.3-.3.6-.6.7-1l.7-3.3c.5-.2.9-.5 1.2-.9.4-.6.7-1.3.7-2-.2-2.5-2-4.3-5-5.1zm-4.1-.2c.1-.2.2-.3.3-.4.1-.1.3-.2.4-.3.2-.1.3-.1.5-.1s.4 0 .5.1c.2.1.3.2.4.3.1.1.2.3.3.4.1.2.1.3.1.5s0 .4-.1.5c0 .2-.1.3-.2.5-.1.1-.3.2-.4.3-.3.1-.7.1-1.1 0-.2-.1-.3-.2-.4-.3-.1-.1-.2-.3-.3-.4-.1-.2-.1-.3-.1-.5-.1-.3 0-.5.1-.6zM8.4 19.2c-.1 0-.1-.1-.1-.2l-.6-2.7c.4 0 .8-.1 1.1-.3 0 0 .1 0 .1-.1l.5 3.3h-1zm4.5 0H11l-.5-3.2c.5.2 1 .4 1.5.4s1-.1 1.5-.4l-.6 3.2zm2.5 0c-.1.1-.1.1-.2.1h-.8l.5-3.3s.1 0 .1.1c.4.2.7.3 1.1.3l-.6 2.6s0 .1-.1.2zm2.3-5c-.2.3-.6.5-1 .6-.4.1-.8 0-1.1-.1-.4-.2-.6-.4-.8-.8-.1-.3-.4-.4-.7-.4-.3 0-.5.2-.7.4-.1.3-.4.5-.6.7-.5.3-1.3.3-1.8 0-.3-.2-.5-.4-.6-.7-.1-.3-.4-.4-.7-.4-.3 0-.5.2-.7.4-.2.3-.5.6-.8.8-.4.2-.8.2-1.1.1-.4-.1-.7-.3-1-.6s-.4-.7-.4-1.1c0-2.3 2.2-3.2 3.6-3.6.1.3.3.6.6.8.3.3.6.5.9.6.4.1.7.2 1.1.2.4 0 .8-.1 1.1-.2s.7-.4.9-.6c.2-.2.4-.5.6-.8 1.3.4 3.6 1.4 3.6 3.6 0 .4-.1.8-.4 1.1z"></path>
-                    </svg> ';
-          break;
-        case 'berry':
-          $svg = '<svg class="align-svg" width="20" height="20">
-                        <path d="M18.3 13.7c-.5-.8-1.3-1.3-2.2-1.6-.3-.1-.6-.2-.9-.2-.9-2.3 0-4.4.9-6.6.1-.3.3-.6.4-.9.1-.3 0-.6-.1-.8-.2-.2-.5-.3-.8-.2-4.6 1.2-7.4 4.1-8.5 8.7-.5.2-1.1.6-1.5 1-.8.8-1.3 2-1.3 3.2s.5 2.3 1.3 3.2c.8.8 2 1.3 3.2 1.3 1.1 0 2.1-.4 2.9-1.1.5.4 1.1.7 1.7.9.4.1.8.2 1.3.2s1-.1 1.5-.2c.9-.3 1.6-.9 2.2-1.6.5-.8.8-1.7.8-2.6-.1-1.1-.4-2-.9-2.7zm-4-8.4c-.8 2-1.5 4.2-.8 6.7h-.2c-.6.2-1.2.5-1.7.9-.8-.7-1.8-1.1-2.9-1.1.9-3.2 2.8-5.3 5.6-6.5zM6.6 18.4c-.6-.6-.9-1.3-.9-2.1s.3-1.5.9-2.1c.6-.6 1.3-.9 2.1-.9s1.5.3 2.1.9.9 1.3.9 2.1-.3 1.5-.9 2.1c-1.1 1.1-3 1.1-4.2 0zM17 18c-.4.5-.9.9-1.5 1.1-.6.2-1.2.2-1.8 0-.4-.1-.8-.3-1.2-.6.4-.7.6-1.4.6-2.2 0-.8-.2-1.5-.6-2.2.3-.3.7-.5 1.2-.6.6-.2 1.2-.2 1.8 0 .6.2 1.1.6 1.5 1.1.4.5.6 1.1.6 1.7 0 .6-.2 1.2-.6 1.7z"></path>
-                    </svg> ';
-          break;
-        case 'hcf_flav_3':
-          $svg = '<svg class="align-svg" width="20" height="20">
-                        <!-- SVG code for flavor 3 goes here -->
-                    </svg> ';
-          break;
-        // Add more cases as needed...
-        default:
-          $svg = '';
-      }
+      $svg = displaySVGFiles($field_value);
 
       $table .= '<td>' . $svg . $field_value . '</td>';
     }
@@ -202,4 +212,16 @@ function prefix_generate_table_3($fields)
   $table .= '</tr></tbody></table>';
 
   return $table;
+}
+
+function cat_is_strains()
+{
+  $cats = get_the_category();
+  foreach ($cats as $cat) {
+    if (strtolower($cat->name) == 'strains') {
+      $isStrains = true;
+    }
+  }
+  return $isStrains;
+
 }
